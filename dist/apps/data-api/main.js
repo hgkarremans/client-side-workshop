ab9ff0075f2f7912a04b13063dec7505a743cbc3
hgkarremans <hg.karremans@student.avans.nl>
2023-11-08 11:33:18 +0100

    pushing with dist

---
+++ dist/apps/data-api/main.js
@@ -0,0 +1,530 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/common");
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/core");
+
+/***/ }),
+/* 3 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AppModule = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const features_1 = __webpack_require__(5);
+const app_controller_1 = __webpack_require__(19);
+const app_service_1 = __webpack_require__(20);
+let AppModule = exports.AppModule = class AppModule {
+};
+exports.AppModule = AppModule = tslib_1.__decorate([
+    (0, common_1.Module)({
+        imports: [features_1.MealModule],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+
+
+/***/ }),
+/* 4 */
+/***/ ((module) => {
+
+module.exports = require("tslib");
+
+/***/ }),
+/* 5 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const tslib_1 = __webpack_require__(4);
+tslib_1.__exportStar(__webpack_require__(6), exports);
+
+
+/***/ }),
+/* 6 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.MealModule = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const meal_controller_1 = __webpack_require__(7);
+const meal_service_1 = __webpack_require__(8);
+let MealModule = exports.MealModule = class MealModule {
+};
+exports.MealModule = MealModule = tslib_1.__decorate([
+    (0, common_1.Module)({
+        controllers: [meal_controller_1.MealController],
+        providers: [meal_service_1.MealService],
+        exports: [meal_service_1.MealService],
+    })
+], MealModule);
+
+
+/***/ }),
+/* 7 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a, _b, _c, _d;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.MealController = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const meal_service_1 = __webpack_require__(8);
+const common_2 = __webpack_require__(1);
+const api_1 = __webpack_require__(10);
+const dto_1 = __webpack_require__(13);
+let MealController = exports.MealController = class MealController {
+    constructor(mealService) {
+        this.mealService = mealService;
+    }
+    getAll() {
+        return this.mealService.getAll();
+    }
+    getOne(id) {
+        return this.mealService.getOne(id);
+    }
+    create(data) {
+        return this.mealService.create(data);
+    }
+};
+tslib_1.__decorate([
+    (0, common_2.Get)(''),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", []),
+    tslib_1.__metadata("design:returntype", Array)
+], MealController.prototype, "getAll", null);
+tslib_1.__decorate([
+    (0, common_2.Get)(':id'),
+    tslib_1.__param(0, (0, common_2.Param)('id')),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [String]),
+    tslib_1.__metadata("design:returntype", typeof (_b = typeof api_1.IMeal !== "undefined" && api_1.IMeal) === "function" ? _b : Object)
+], MealController.prototype, "getOne", null);
+tslib_1.__decorate([
+    (0, common_2.Post)(''),
+    tslib_1.__param(0, (0, common_2.Body)()),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [typeof (_c = typeof dto_1.CreateMealDto !== "undefined" && dto_1.CreateMealDto) === "function" ? _c : Object]),
+    tslib_1.__metadata("design:returntype", typeof (_d = typeof api_1.IMeal !== "undefined" && api_1.IMeal) === "function" ? _d : Object)
+], MealController.prototype, "create", null);
+exports.MealController = MealController = tslib_1.__decorate([
+    (0, common_1.Controller)('meal'),
+    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof meal_service_1.MealService !== "undefined" && meal_service_1.MealService) === "function" ? _a : Object])
+], MealController);
+
+
+/***/ }),
+/* 8 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.MealService = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const rxjs_1 = __webpack_require__(9);
+const common_2 = __webpack_require__(1);
+const api_1 = __webpack_require__(10);
+let MealService = exports.MealService = class MealService {
+    constructor() {
+        this.TAG = 'MealService';
+        this.meals$ = new rxjs_1.BehaviorSubject([
+            {
+                id: '0',
+                title: 'Spaghetti con funghi',
+                description: 'Vega version of the famous spaghetti recipe.',
+                isVega: true,
+                sort: api_1.MealSort.Breakfast,
+                cook: "quinn",
+                dateServed: new Date(),
+            },
+        ]);
+    }
+    getAll() {
+        common_2.Logger.log('getAll', this.TAG);
+        return this.meals$.value;
+    }
+    getOne(id) {
+        common_2.Logger.log(`getOne(${id})`, this.TAG);
+        const meal = this.meals$.value.find((td) => td.id === id);
+        if (!meal) {
+            throw new common_1.NotFoundException(`Meal could not be found!`);
+        }
+        return meal;
+    }
+    /**
+     * Update the arg signature to match the DTO, but keep the
+     * return signature - we still want to respond with the complete
+     * object
+     */
+    create(meal) {
+        common_2.Logger.log('create', this.TAG);
+        const current = this.meals$.value;
+        // Use the incoming data, a randomized ID, and a default value of `false` to create the new to-do
+        const newMeal = {
+            ...meal,
+            id: `meal-${Math.floor(Math.random() * 10000)}`,
+            isVega: false,
+            sort: api_1.MealSort.Breakfast,
+            cook: "quinn",
+            dateServed: new Date(),
+        };
+        this.meals$.next([...current, newMeal]);
+        return newMeal;
+    }
+};
+exports.MealService = MealService = tslib_1.__decorate([
+    (0, common_1.Injectable)()
+], MealService);
+
+
+/***/ }),
+/* 9 */
+/***/ ((module) => {
+
+module.exports = require("rxjs");
+
+/***/ }),
+/* 10 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const tslib_1 = __webpack_require__(4);
+tslib_1.__exportStar(__webpack_require__(11), exports);
+tslib_1.__exportStar(__webpack_require__(12), exports);
+
+
+/***/ }),
+/* 11 */
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.MealSort = void 0;
+var MealSort;
+(function (MealSort) {
+    MealSort["Breakfast"] = "Breakfast";
+    MealSort["Lunch"] = "Lunch";
+    MealSort["Dinner"] = "Dinner";
+    MealSort["Other"] = "Other";
+})(MealSort || (exports.MealSort = MealSort = {}));
+
+
+/***/ }),
+/* 12 */
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+
+
+/***/ }),
+/* 13 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const tslib_1 = __webpack_require__(4);
+tslib_1.__exportStar(__webpack_require__(14), exports);
+tslib_1.__exportStar(__webpack_require__(15), exports);
+tslib_1.__exportStar(__webpack_require__(17), exports);
+
+
+/***/ }),
+/* 14 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.DtoModule = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+let DtoModule = exports.DtoModule = class DtoModule {
+};
+exports.DtoModule = DtoModule = tslib_1.__decorate([
+    (0, common_1.Module)({
+        controllers: [],
+        providers: [],
+        exports: [],
+    })
+], DtoModule);
+
+
+/***/ }),
+/* 15 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a, _b, _c;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.UpdateMealDto = exports.UpsertMealDto = exports.CreateMealDto = void 0;
+const tslib_1 = __webpack_require__(4);
+const class_validator_1 = __webpack_require__(16);
+const api_1 = __webpack_require__(10);
+/**
+ * Use the `Pick` utility type to extract only the properties we want for
+ * new to-do items
+ */
+class CreateMealDto {
+}
+exports.CreateMealDto = CreateMealDto;
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], CreateMealDto.prototype, "title", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], CreateMealDto.prototype, "description", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", typeof (_a = typeof api_1.MealSort !== "undefined" && api_1.MealSort) === "function" ? _a : Object)
+], CreateMealDto.prototype, "sort", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], CreateMealDto.prototype, "cook", void 0);
+class UpsertMealDto {
+}
+exports.UpsertMealDto = UpsertMealDto;
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], UpsertMealDto.prototype, "title", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], UpsertMealDto.prototype, "description", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], UpsertMealDto.prototype, "id", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsBoolean)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", Boolean)
+], UpsertMealDto.prototype, "isVega", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsDate)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", typeof (_b = typeof Date !== "undefined" && Date) === "function" ? _b : Object)
+], UpsertMealDto.prototype, "dateServed", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", typeof (_c = typeof api_1.MealSort !== "undefined" && api_1.MealSort) === "function" ? _c : Object)
+], UpsertMealDto.prototype, "sort", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsNotEmpty)(),
+    tslib_1.__metadata("design:type", String)
+], UpsertMealDto.prototype, "cook", void 0);
+class UpdateMealDto {
+}
+exports.UpdateMealDto = UpdateMealDto;
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsOptional)(),
+    tslib_1.__metadata("design:type", String)
+], UpdateMealDto.prototype, "title", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.IsOptional)(),
+    tslib_1.__metadata("design:type", String)
+], UpdateMealDto.prototype, "description", void 0);
+tslib_1.__decorate([
+    (0, class_validator_1.IsBoolean)(),
+    (0, class_validator_1.IsOptional)(),
+    tslib_1.__metadata("design:type", Boolean)
+], UpdateMealDto.prototype, "completed", void 0);
+
+
+/***/ }),
+/* 16 */
+/***/ ((module) => {
+
+module.exports = require("class-validator");
+
+/***/ }),
+/* 17 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.ApiResponseInterceptor = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const operators_1 = __webpack_require__(18);
+let ApiResponseInterceptor = exports.ApiResponseInterceptor = class ApiResponseInterceptor {
+    intercept(context, next) {
+        return next.handle().pipe((0, operators_1.map)((results) => {
+            if (results) {
+                return {
+                    results,
+                    info: {
+                        version: '1.0',
+                        type: results instanceof Array ? 'list' : 'object',
+                        count: results instanceof Array ? results.length : 1,
+                    },
+                };
+            }
+            else {
+                return {
+                    results: undefined,
+                    info: {
+                        version: '1.0',
+                        type: 'none',
+                        count: 0,
+                    },
+                };
+            }
+        }));
+    }
+};
+exports.ApiResponseInterceptor = ApiResponseInterceptor = tslib_1.__decorate([
+    (0, common_1.Injectable)()
+], ApiResponseInterceptor);
+
+
+/***/ }),
+/* 18 */
+/***/ ((module) => {
+
+module.exports = require("rxjs/operators");
+
+/***/ }),
+/* 19 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AppController = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+const app_service_1 = __webpack_require__(20);
+let AppController = exports.AppController = class AppController {
+    constructor(appService) {
+        this.appService = appService;
+    }
+    getData() {
+        return this.appService.getData();
+    }
+};
+tslib_1.__decorate([
+    (0, common_1.Get)(),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", []),
+    tslib_1.__metadata("design:returntype", void 0)
+], AppController.prototype, "getData", null);
+exports.AppController = AppController = tslib_1.__decorate([
+    (0, common_1.Controller)(),
+    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof app_service_1.AppService !== "undefined" && app_service_1.AppService) === "function" ? _a : Object])
+], AppController);
+
+
+/***/ }),
+/* 20 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AppService = void 0;
+const tslib_1 = __webpack_require__(4);
+const common_1 = __webpack_require__(1);
+let AppService = exports.AppService = class AppService {
+    getData() {
+        return { message: 'Hello API' };
+    }
+};
+exports.AppService = AppService = tslib_1.__decorate([
+    (0, common_1.Injectable)()
+], AppService);
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const common_1 = __webpack_require__(1);
+const core_1 = __webpack_require__(2);
+const app_module_1 = __webpack_require__(3);
+const dto_1 = __webpack_require__(13);
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
+    const corsOptions = {};
+    app.enableCors(corsOptions);
+    app.useGlobalInterceptors(new dto_1.ApiResponseInterceptor());
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    common_1.Logger.log(`🚀 Application: data-api is running on: http://localhost:${port}/${globalPrefix}`);
+}
+bootstrap();
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
