000093a67a68a05716b74bd2b9149b3811774c3f
hgkarremans <hg.karremans@student.avans.nl>
2023-11-21 10:20:26 +0100

    fixed create user functionality

--- libs/share-a-meal/user/src/lib/user-create/user-create.component.ts
+++ libs/share-a-meal/user/src/lib/user-create/user-create.component.ts
@@ -2,38 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
 import { User } from '@avans-nx-workshop/shared/api';
-import { UserGender } from '@avans-nx-workshop/shared/api';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { UserRole } from '@avans-nx-workshop/shared/api';
-
+import { DeleteConformationModalComponent } from '../delete-conformation-modal/delete-conformation-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'clientside-nx-workshop-user-create',
   templateUrl: './user-create.component.html',
   styles: [],
 })
 export class UserCreateComponent {
-  user!: User;
-  userForm!: FormGroup;
-  genderOptions = Object.values(UserGender);
-  rolesOptions = Object.values(UserRole);
+  user!: User; // Add definite assignment assertion
 
-  constructor(
-    private route: ActivatedRoute,
-    private userService: UserService,
-    private fb: FormBuilder
-  ) {
-    this.userForm = this.fb.group({
-      gender: [''], 
-      role: [''],
-    });
-  }
+  constructor(private route: ActivatedRoute, private userService: UserService, private modalService: NgbModal) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.userForm.patchValue({
-        gender: this.user.gender,
-        role: this.user.role,
-      });
+    this.route.paramMap.subscribe(params => {
+      const userId = Number(params.get('id'));
+      this.user = this.userService.getUserById(userId);
     });
   }
+  
 }
