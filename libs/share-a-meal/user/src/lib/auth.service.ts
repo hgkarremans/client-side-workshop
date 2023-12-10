c102cd36c78cff34931f521ef7f53a9769330e88
hgkarremans <hg.karremans@student.avans.nl>
2023-12-10 13:11:22 +0100

    live login and logout

--- libs/share-a-meal/user/src/lib/auth.service.ts
+++ libs/share-a-meal/user/src/lib/auth.service.ts
@@ -1,11 +1,14 @@
-// auth.service.ts
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly TOKEN_KEY = 'access_token';
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
+
+  isLoggedIn$ = this.isLoggedInSubject.asObservable();
 
   getToken(): string | null {
     return localStorage.getItem(this.TOKEN_KEY);
@@ -13,10 +16,12 @@ export class AuthService {
 
   setToken(token: string): void {
     localStorage.setItem(this.TOKEN_KEY, token);
+    this.isLoggedInSubject.next(true);
   }
 
   removeToken(): void {
     localStorage.removeItem(this.TOKEN_KEY);
+    this.isLoggedInSubject.next(false);
   }
 
   isLoggedIn(): boolean {
