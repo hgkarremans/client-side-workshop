a6ab812e1cfe1c74206a064582d6f8a1ffa58340
hgkarremans <hg.karremans@student.avans.nl>
2023-12-13 11:49:49 +0100

    neo4j

--- libs/shared/env/src/lib/environment.production.ts
+++ libs/shared/env/src/lib/environment.production.ts
@@ -4,7 +4,7 @@ export const environment: IEnvironment = {
   production: true,
   apiUrl: 'https://clientsidehg.azurewebsites.net/api/',  
   MONGO_DB_CONNECTION_STRING: 'mongodb+srv://hgkarremans:ticketshop2003@ticketshop.tof9tyt.mongodb.net/?retryWrites=true&w=majority',  
-  NEO4J_SCHEME: 'neo4j+s',
+  NEO4J_SCHEME: 'neo4j',
   NEO4J_HOST: 'neo4j+s://4127d9b6.databases.neo4j.io',  
   NEO4J_PORT: '7687',  
   NEO4J_USERNAME: 'neo4j', 
