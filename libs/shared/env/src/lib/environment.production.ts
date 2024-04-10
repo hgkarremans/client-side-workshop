887f29834f3b9da021c8030adf90f03045d6b0a6
hgkarremans <hg.karremans@student.avans.nl>
2024-04-10 13:40:31 +0200

    trying to fix neo4j

--- libs/shared/env/src/lib/environment.production.ts
+++ libs/shared/env/src/lib/environment.production.ts
@@ -5,9 +5,9 @@ export const environment: IEnvironment = {
   apiUrl: 'https://clientsidehg.azurewebsites.net/api/',  
   MONGO_DB_CONNECTION_STRING: 'mongodb+srv://hgkarremans:ticketshop2003@ticketshop.tof9tyt.mongodb.net/?retryWrites=true&w=majority',  
   NEO4J_SCHEME: 'neo4j',
-  NEO4J_HOST: 'neo4j+s://4127d9b6.databases.neo4j.io',  
+  NEO4J_HOST: 'neo4j+s://34280323.databases.neo4j.io',  
   NEO4J_PORT: '7687',  
   NEO4J_USERNAME: 'neo4j', 
-  NEO4J_PASSWORD: 'SmJLgg2CZAc0Dg7V45h_wtLcvz8GsGOy6_4NL3Q5H_8',  
-  NEO4J_DATABASE: 'ticketshop',   
+  NEO4J_PASSWORD: 'Teg2MDY5jsmvbwaI-522HGvJJuSuALqFkWTBxdIqzI4',  
+  NEO4J_DATABASE: 'neo4j',   
 };
